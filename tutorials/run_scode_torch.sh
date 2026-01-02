bc4b8a30783eec1739aa65293d6254de2af9b67d
rbsung <rakbin007@naver.com>
2026-01-02 15:22:03 +0900

    update tutorials

--- tutorials/run_scode_torch.sh
+++ tutorials/run_scode_torch.sh
@@ -7,6 +7,13 @@ python run_scode.py --droot . \
                     --backend gpu \
                     --num_devices 8 \
                     --batch_size_b 100 \
-                    --batch_size 4000 \
                     --sp_droot out \
                     --num_repeat 6
+
+python reconstruct_grn.py \
+ --fp_rm score_result_matrix.txt \
+ --fp_gn node_name.txt \
+ --fp_tf mouse_tf.txt \
+ --fdr 0.01 \
+ --backend gpu \
+ --device_ids 1
