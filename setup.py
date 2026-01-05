6bd669a9397211a2b47784e4cfad435cd29b7790
rbsung <rakbin007@naver.com>
2026-01-05 16:25:44 +0900

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='fastscode',
-      version='0.0.7',
+      version='0.0.8',
       description='FastSCODE',
       # url='http://github.com/cxinsys/fasttenet',
       author='Complex Intelligent Systems Laboratory (CISLAB)',
